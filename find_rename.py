7b867979982b652aeaf0489aba68b94e32f401b9
Sunil <sunilkamatec@gmail.com>
2018-08-26 11:53:44 +0530

    working

--- find_rename.py
+++ find_rename.py
@@ -4,5 +4,7 @@ for name in glob.glob('/root/shri/distribution/folds/*'):
    if os.path.isdir(name):
          for i in glob.glob('{}/*'.format(name)):
               new_name = os.path.split(i)[1].split('-')[0] + '.latest.ear'
-              print(i)
-              print(new_name)
+              #print(i)
+              #print(new_name)
+              #print(os.path.split(i)[0])
+              os.rename(i, '{}/{}'.format(os.path.split(i)[0], new_name))
