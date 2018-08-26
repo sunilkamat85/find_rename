e3f94218dda9f493c150b6595284e4387de816bc
Sunil <sunilkamatec@gmail.com>
2018-08-26 11:14:56 +0530

    folder struct canged

--- find_rename.py
+++ find_rename.py
@@ -1,6 +1,6 @@
 import os
 import glob
-for name in glob.glob('/root/shri/distribution/*'):
+for name in glob.glob('/root/shri/distribution/folds/*'):
    if os.path.isdir(name):
          for i in glob.glob('{}/*'.format(name)):
               print(i)
